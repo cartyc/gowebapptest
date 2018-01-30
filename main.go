9fa2dde095723b3e9391218eb14a56509c664c58
Chris Carty <christopher.carty@ottawa.ca>
2018-01-30 10:22:32 -0500

    added static files

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
+	"log"
+	"io"
+	"strings"
 )
 
-type myHandler struct {
-	greeting string
-}
-
-func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
-}
 
 func main(){
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
+		f, err := os.Open("public" + r.URL.Path)
 
-		http.Handle("/", &myHandler{greeting: "Hello"})
-		http.ListenAndServe(":8080", nil)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+		}
 
+		defer f.Close()
 
+		var contentType string
+		switch  {
+		case strings.HasSuffix(r.URL.Path, "css"):
+			contentType = "text/css"
+		case strings.HasSuffix(r.URL.Path, "html"):
+			contentType = "text/html"
+		case strings.HasSuffix(r.URL.Path, "png"):
+			contentType = "image/png"
+		default:
+			contentType = "text/plain"
+		}
 
+		w.Header().Add("Content-Type", contentType)
+		io.Copy(w, f)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
