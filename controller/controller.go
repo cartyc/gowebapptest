a9c89085a00119d7e2909b106aab0e23e49c03f1
Chris Carty <chriscartydev@gmail.com>
2018-02-05 15:05:41 -0500

    more routing

---
+++ controller/controller.go
@@ -0,0 +1,25 @@
+package controller
+
+import(
+	"html/template"
+	"net/http"
+)
+
+var(
+	homeController home
+	shopController shop
+)
+
+func Startup (templates map[string]*template.Template){
+
+	homeController.homeTemplate = templates["home.html"]
+	homeController.registerRoutes()
+
+	shopController.shopTemplate = templates["shop.html"]
+	shopController.registerRoutes()
+
+	shopController.categoryTemplate = templates["shop_details.html"]
+
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+}
